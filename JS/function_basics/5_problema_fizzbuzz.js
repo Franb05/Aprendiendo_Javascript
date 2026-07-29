4602abdc9a634aad951b35eb3073a9107cb2d721
Franb05 <146026328+Franb05@users.noreply.github.com>
2026-07-28 22:01:11 -0300

    Organize exercises and solve FizzBuzz exercise

---
+++ JS/function_basics/5_problema_fizzbuzz.js
@@ -0,0 +1,22 @@
+/**Escribe un programa que tome la entrada del usuario e imprima los números
+ *  del uno al número que el usuario ingresó. Sin embargo, para los múltiplos
+ *  de tres, imprime Fizzen lugar del número y para los múltiplos de cinco,
+ * imprime Buzz. Para los números que son múltiplos tanto de tres como de cinco, imprime FizzBuzz
+ */
+
+let numero = Number(prompt("Ingrese un numero"));
+let contador = 0;
+while (contador < numero) {
+  if (contador % 3 === 0 && contador % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (contador % 3 === 0) {
+    console.log("Fizz");
+  } else if (contador % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(contador);
+  }
+
+  contador++;
+}
+console.log(contador);
